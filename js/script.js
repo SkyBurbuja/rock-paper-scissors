0b7984dfa461fd702cdd5d0338ed07bd73c9dc8c
SkyBurbuja <kingperez.kingvera@gmail.com>
2021-03-03 01:29:27 -0600

    Rename html to index

--- js/script.js
+++ js/script.js
@@ -61,10 +61,12 @@
         return 'That is not a valid response please try again';
       }
 
+      //Record is initially declared
       let wins =0;
       let losses=0;
       let ties=0;
 
+      //Keeps track of wins or losses
       function winOrLose(result){
         let winOrLose;
 
